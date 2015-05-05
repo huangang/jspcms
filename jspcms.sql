a3f3286d0ce91673ec27d94971ae2e0910215067
huangang <huangangzhang@gmail.com>
2015-05-05 11:48:02 +0800

    databases link demo

--- jspcms.sql
+++ jspcms.sql
@@ -2,64 +2,116 @@
 *jspcms
 *内容管理系统
 */
-create table `users`
-(
+--
+-- Database: `jspcms`
+--
 
-	`uid` int(8) NOT NULL comment '用户id',
-	`username` varchar(255) NOT NULL comment '用户名',
-	`password` varchar(255) NOT NULL comment '密码',
-	`email` varchar(255) NOT NULL comment '邮箱',
-	`role` varchar(11) NOT NULL comment '身份',
-	`register_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' comment '注册时间',
-	primary key (uid)
+-- --------------------------------------------------------
 
-)ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-alter table users comment '用户表';
+--
+-- 表的结构 `comments`
+--
 
+CREATE TABLE `comments` (
+	`cid` int(8) NOT NULL COMMENT '评论id',
+	`pid` int(8) NOT NULL COMMENT '文章id',
+	`uid` int(8) NOT NULL COMMENT '用户id',
+	`reply_cid` int(8) DEFAULT NULL COMMENT '回复评论id',
+	`content` longtext NOT NULL COMMENT '评论内容',
+	`comment_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '评论时间'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='评论表';
 
-create table `posts`
-(
+-- --------------------------------------------------------
 
-`pid` int(8) NOT NULL comment '文章id',
-`uid` int(8) NOT NULL comment '用户id',
-`sid` int(8) NOT NULL comment '分类id',
-`title` text NOT NULL comment '文章标题',
-`content` longtext NOT NULL comment '文章内容',
-`post_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' comment '发布时间',
-`have_comment` boolean NOT NULL DEFAULT true comment '是否评论',
-primary key (pid)
+--
+-- 表的结构 `posts`
+--
 
-)ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-alter table posts comment '文章表';
+CREATE TABLE `posts` (
+	`pid` int(8) NOT NULL COMMENT '文章id',
+	`uid` int(8) NOT NULL COMMENT '用户id',
+	`sid` int(8) NOT NULL COMMENT '分类id',
+	`title` text NOT NULL COMMENT '文章标题',
+	`content` longtext NOT NULL COMMENT '文章内容',
+	`post_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '发布时间',
+	`have_comment` tinyint(1) NOT NULL DEFAULT '1' COMMENT '是否评论'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='文章表';
 
+-- --------------------------------------------------------
 
-create table `comments`
-(
+--
+-- 表的结构 `sorts`
+--
 
-`cid` int(8) NOT NULL comment '评论id',
-`pid` int(8) NOT NULL comment '文章id',
-`uid` int(8) NOT NULL comment '用户id',
-`reply_cid` int(8) comment '回复评论id',
-`content` longtext NOT NULL comment '评论内容',
-`comment_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' comment '评论时间',
-primary key (cid)
+CREATE TABLE `sorts` (
+	`sid` int(8) NOT NULL COMMENT '分类id',
+	`sname` varchar(255) NOT NULL COMMENT '分类名'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='文章分类表';
 
-)ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-alter table comments comment '评论表';
+-- --------------------------------------------------------
 
+--
+-- 表的结构 `users`
+--
 
+CREATE TABLE `users` (
+	`uid` int(8) NOT NULL COMMENT '用户id',
+	`username` varchar(255) NOT NULL COMMENT '用户名',
+	`password` varchar(255) NOT NULL COMMENT '密码',
+	`email` varchar(255) NOT NULL COMMENT '邮箱',
+	`role` varchar(11) NOT NULL COMMENT '身份',
+	`register_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '注册时间'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='用户表';
 
-create table `sorts`
-( 
+--
+-- Indexes for dumped tables
+--
 
-`sid` int(8) NOT NULL comment '分类id',
-`sname` varchar(255) NOT NULL comment '分类名',
-primary key (sid)
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+ADD PRIMARY KEY (`cid`);
 
-)ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+ADD PRIMARY KEY (`pid`);
 
-alter table sorts comment '文章分类表';
+--
+-- Indexes for table `sorts`
+--
+ALTER TABLE `sorts`
+ADD PRIMARY KEY (`sid`);
 
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ADD PRIMARY KEY (`uid`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
-
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+MODIFY `cid` int(8) NOT NULL AUTO_INCREMENT COMMENT '评论id';
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+MODIFY `pid` int(8) NOT NULL AUTO_INCREMENT COMMENT '文章id';
+--
+-- AUTO_INCREMENT for table `sorts`
+--
+ALTER TABLE `sorts`
+MODIFY `sid` int(8) NOT NULL AUTO_INCREMENT COMMENT '分类id';
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+MODIFY `uid` int(8) NOT NULL AUTO_INCREMENT COMMENT '用户id';
--- out/artifacts/jspcms_war_exploded/index.jsp
+++ out/artifacts/jspcms_war_exploded/index.jsp
@@ -6,11 +6,28 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page import="java.sql.*" %>
 <html>
   <head>
     <title></title>
   </head>
   <body>
   <h1>hello jspcms</h1>
+<%
+  String driverClass="com.mysql.jdbc.Driver";
+  String url="jdbc:mysql://localhost:3306/jspcms";
+  String username = "root";
+  String password = "64314527Z";
+  Class.forName(driverClass);	 // 加载数据库驱动
+  Connection conn=DriverManager.getConnection(url, username, password);	//建立连接
+  Statement stmt=conn.createStatement();
+  ResultSet rs = stmt.executeQuery("select * from users");	//执行SQL语句
+  while(rs.next()){
+    out.println("<br>用户名："+rs.getString(2)+"	密码："+rs.getString(3));
+  }
+  rs.close();
+  stmt.close();
+  conn.close();
+%>
   </body>
 </html>
