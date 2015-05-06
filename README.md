998961cc2724c99077c42966c53605b2feb1f7aa
huangang <huangangzhang@gmail.com>
2015-05-07 07:32:47 +0800

    sql readme update

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 #jspcms
 ##jsp内容管理系统
+##测试用户名:admin,密码:123456
+##[演示站点](http://jspcms.jd-app.com/)
 
--- jspcms.sql
+++ jspcms.sql
@@ -7,19 +7,19 @@
 --
 
 -- --------------------------------------------------------
-
 --
 -- 表的结构 `comments`
 --
 
-CREATE TABLE `comments` (
-	`cid` int(8) NOT NULL COMMENT '评论id',
+CREATE TABLE IF NOT EXISTS `comments` (
+	`cid` int(8) NOT NULL AUTO_INCREMENT COMMENT '评论id',
 	`pid` int(8) NOT NULL COMMENT '文章id',
 	`uid` int(8) NOT NULL COMMENT '用户id',
 	`reply_cid` int(8) DEFAULT NULL COMMENT '回复评论id',
 	`content` longtext NOT NULL COMMENT '评论内容',
-	`comment_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '评论时间'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='评论表';
+	`comment_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '评论时间',
+	PRIMARY KEY (`cid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='评论表' AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -27,15 +27,16 @@ CREATE TABLE `comments` (
 -- 表的结构 `posts`
 --
 
-CREATE TABLE `posts` (
-	`pid` int(8) NOT NULL COMMENT '文章id',
+CREATE TABLE IF NOT EXISTS `posts` (
+	`pid` int(8) NOT NULL AUTO_INCREMENT COMMENT '文章id',
 	`uid` int(8) NOT NULL COMMENT '用户id',
 	`sid` int(8) NOT NULL COMMENT '分类id',
 	`title` text NOT NULL COMMENT '文章标题',
 	`content` longtext NOT NULL COMMENT '文章内容',
 	`post_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '发布时间',
-	`is_have_comment` tinyint(1) NOT NULL DEFAULT '1' COMMENT '是否评论'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='文章表';
+	`is_have_comment` tinyint(1) NOT NULL DEFAULT '1' COMMENT '是否评论',
+	PRIMARY KEY (`pid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='文章表' AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -43,10 +44,18 @@ CREATE TABLE `posts` (
 -- 表的结构 `sorts`
 --
 
-CREATE TABLE `sorts` (
-	`sid` int(8) NOT NULL COMMENT '分类id',
-	`sname` varchar(255) NOT NULL COMMENT '分类名'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='文章分类表';
+CREATE TABLE IF NOT EXISTS `sorts` (
+	`sid` int(8) NOT NULL AUTO_INCREMENT COMMENT '分类id',
+	`sname` varchar(255) NOT NULL COMMENT '分类名',
+	PRIMARY KEY (`sid`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='文章分类表' AUTO_INCREMENT=2 ;
+
+--
+-- 转存表中的数据 `sorts`
+--
+
+INSERT INTO `sorts` (`sid`, `sname`) VALUES
+	(1, '默认分类');
 
 -- --------------------------------------------------------
 
@@ -54,64 +63,19 @@ CREATE TABLE `sorts` (
 -- 表的结构 `users`
 --
 
-CREATE TABLE `users` (
-	`uid` int(8) NOT NULL COMMENT '用户id',
+CREATE TABLE IF NOT EXISTS `users` (
+	`uid` int(8) NOT NULL AUTO_INCREMENT COMMENT '用户id',
 	`username` varchar(255) NOT NULL COMMENT '用户名',
 	`password` varchar(255) NOT NULL COMMENT '密码',
 	`email` varchar(255) NOT NULL COMMENT '邮箱',
 	`role` varchar(11) NOT NULL COMMENT '身份',
-	`register_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '注册时间'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='用户表';
+	`register_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '注册时间',
+	PRIMARY KEY (`uid`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='用户表' AUTO_INCREMENT=2 ;
 
 --
--- Indexes for dumped tables
+-- 转存表中的数据 `users`
 --
 
---
--- Indexes for table `comments`
---
-ALTER TABLE `comments`
-ADD PRIMARY KEY (`cid`);
-
---
--- Indexes for table `posts`
---
-ALTER TABLE `posts`
-ADD PRIMARY KEY (`pid`);
-
---
--- Indexes for table `sorts`
---
-ALTER TABLE `sorts`
-ADD PRIMARY KEY (`sid`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-ADD PRIMARY KEY (`uid`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `comments`
---
-ALTER TABLE `comments`
-MODIFY `cid` int(8) NOT NULL AUTO_INCREMENT COMMENT '评论id';
---
--- AUTO_INCREMENT for table `posts`
---
-ALTER TABLE `posts`
-MODIFY `pid` int(8) NOT NULL AUTO_INCREMENT COMMENT '文章id';
---
--- AUTO_INCREMENT for table `sorts`
---
-ALTER TABLE `sorts`
-MODIFY `sid` int(8) NOT NULL AUTO_INCREMENT COMMENT '分类id';
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-MODIFY `uid` int(8) NOT NULL AUTO_INCREMENT COMMENT '用户id';
+INSERT INTO `users` (`uid`, `username`, `password`, `email`, `role`, `register_time`) VALUES
+	(1, 'admin', '123456', '123456@qq.com', 'manager', '2015-05-07 07:22:06');
